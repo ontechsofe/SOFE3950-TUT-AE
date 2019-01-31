2f48f7680d75c6678e711cc928c3099c8d263bbd
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-01-31 14:21:56 -0500

    question1

---
+++ Tut3/question1.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main(void)
+{
+    char* name;
+    int age, height;
+    printf("Please enter first name: ");
+    scanf("%s", name);
+    printf("Please enter age: ");
+    scanf("%d", &age);
+    printf("Please enter height in centimeters: ");
+    scanf("%d", &height);
+    printf("Hi %s, you are %d, and you are %dcm tall\n", name, age, height);
+}   
